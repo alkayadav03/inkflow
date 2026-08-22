b277848b7656e80757ece1b6c45f2e98544c0d53
Alka <yalka834@gamil.com>
2026-08-23 01:31:09 +0530

    initial commit

---
+++ app/(auth)/sign-in/[[...sign-in]]/page.jsx
@@ -0,0 +1,9 @@
+import { SignIn } from "@clerk/nextjs";
+// import React from "react";
+// const SignIn=() => {
+//     return <div>Sign In</div>
+// };
+export default function Page() {
+  return <SignIn />;
+}
+// export default SignIn;
